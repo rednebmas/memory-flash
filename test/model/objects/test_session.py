88c2b4431f3f545c14e837d2a0cb08759b66f7af
Sam Bender <rednebmas@gmail.com>
2017-06-28 19:45:37 -0700

    Fixed user related unit test errors

--- test/model/objects/test_session.py
+++ test/model/objects/test_session.py
@@ -7,30 +7,32 @@ from model.objects.answer_history import AnswerHistory
 from model.migration_manager import MigrationManager
 import sqlite3
 
+user_id = 1
+
 class TestSession(unittest.TestCase):
 	def setUp(self):
 		db.unittest_reset()
 
 	def test_from_deck_id(self):
-		sesh = Session.from_deck_id(1)
+		sesh = Session.from_deck_id(user_id, 1)
 		self.assertTrue(sesh is not None)
 		self.assertTrue(isinstance(sesh.session_id, int))
 
 	def test_cards_is_initially_empty(self):
-		session = Session.from_deck_id(1)
+		session = Session.from_deck_id(user_id, 1)
 		session.load_cards()
 		self.assertEqual(len(session.cards), 0)
 
-		session = Session.from_deck_id(2)
+		session = Session.from_deck_id(user_id, 2)
 		session.load_cards()
 		self.assertEqual(len(session.cards), 0)
 
-		session = Session.from_deck_id(3)
+		session = Session.from_deck_id(user_id, 3)
 		session.load_cards()
 		self.assertEqual(len(session.cards), 0)
 
 	def test_from_db(self):
-		session = Session.from_deck_id(1)
+		session = Session.from_deck_id(user_id, 1)
 		row = db.select1(table="Session", where="deck_id = 1")
 		session_from_db = Session.from_db(row)
 		self.assertEqual(session_from_db.session_id, 1)
@@ -40,33 +42,33 @@ class TestSession(unittest.TestCase):
 		self.assertTrue(session_from_db.median is None)
 
 	def test_update_stage(self):
-		session = Session.from_deck_id(1)
+		session = Session.from_deck_id(user_id, 1)
 		session.update_stage('speed up')
 		row = db.select1(table="Session", where="session_id = ?", substitutions=(session.session_id,))
 		self.assertEqual(row['stage'], 'speed up')
 
 	def test_load_cards_loads_most_recent_answer_history(self):
-		session = Session.from_deck_id(1)
+		session = Session.from_deck_id(user_id, 1)
 		from datetime import datetime, timedelta
 		two_seconds_ago = datetime.now() - timedelta(seconds=2)
 		two_seconds_ago = two_seconds_ago.strftime('%Y-%m-%d %H:%M:%S')
 
 		card_id = db.select1(table="Card", where="deck_id = ?", substitutions=(session.deck_id,))['card_id']
 
-		AnswerHistory(session.session_id, card_id, 1.12, True, two_seconds_ago).insert()
+		AnswerHistory(session.session_id, card_id, user_id, 1.12, True, two_seconds_ago).insert()
 		session.load_cards()
 		self.assertEqual(len(session.cards), 1)
 		self.assertEqual(session.cards[0].answer_history.time_to_correct, 1.12)
 
-		AnswerHistory(session.session_id, card_id, 2.12, True, DB.datetime_now()).insert()
+		AnswerHistory(session.session_id, card_id, user_id, 2.12, True, DB.datetime_now()).insert()
 		session.load_cards()
 		self.assertEqual(len(session.cards), 1)
 		self.assertEqual(session.cards[0].answer_history.time_to_correct, 2.12)
 
 	def test_from_deck_id_gives_new_session_if_stage_is_finished(self):
-		session = Session.from_deck_id(3)
+		session = Session.from_deck_id(user_id, 3)
 		session.update_stage('finished')
-		session2 = Session.from_deck_id(3)
+		session2 = Session.from_deck_id(user_id, 3)
 		self.assertNotEqual(session.session_id, session2.session_id)
 
 
--- test/viewmodel/test_study_view_model.py
+++ test/viewmodel/test_study_view_model.py
@@ -5,7 +5,8 @@ from model.objects.card import Card
 class TestStudyViewModel(unittest.TestCase):
 
 	def test_next_card(self):
-		card, session = StudyViewModel.next_card(1, 1)
+		user_id = 1
+		card, session = StudyViewModel.next_card(user_id, 1, 1)
 		self.assertTrue(isinstance(card, Card), card)
 
 
