ef158c647b22cd8e31bf26a143766cfcd3d0f834
Sam Bender <rednebmas@gmail.com>
2017-07-22 12:41:14 -0700

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
+if ! [ -x "$(command -v ruby)" ]; then
+	sudo apt-get install ruby-dev
+fi
+if ! [ -x "$(command -v sass)" ]; then
+	sudo gem install sass --no-user-install
+fi
+
 sudo git checkout -- .
 sudo git pull
 sudo sed -i "s/8000/80/g" main.py
+sudo npm update
 sudo npm run compile
 sudo python3 main.py
